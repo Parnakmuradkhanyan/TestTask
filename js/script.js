c709742a71f1546229b03eac8978250e7eb22391
Parnak <parnak725@gmail.com>
2025-07-16 18:53:29 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
+
+// Langauge dropdown desktop
+const languageToggle = document.getElementById('languageToggle');
+languageToggle?.addEventListener('click', e => {
+  e.stopPropagation();
+  languageToggle.classList.toggle('open');
+});
+
+// Langauge dropdown phone
+const languageToggleMobile = document.getElementById('languageToggleMobile');
+languageToggleMobile?.addEventListener('click', e => {
+  e.stopPropagation();
+  languageToggleMobile.classList.toggle('open');
+});
+
+// Closing dropdown
+document.addEventListener('click', () => {
+  languageToggle?.classList.remove('open');
+  languageToggleMobile?.classList.remove('open');
+});
